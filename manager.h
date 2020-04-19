c88fca2b2f09f0c44ee2594a139decfb6e37f7c9
Kim YeongEun <sally4386@naver.com>
2020-04-19 20:40:44 +0900

    modify manager.h

--- manager.h
+++ manager.h
@@ -2,7 +2,7 @@
 
 void listProduct(Product *s,int count);//전체 상품 리스트 출력 함수
 int selectProductNo(Product *s,int count);//전체 상품 리스트 중 하나 선택하는 함수
-void savdData(Product *s,int count);//파일 저장하는 함수
+void saveData(Product *s,int count);//파일 저장하는 함수
 int loadData(Product s[]);// 파일 불러오는 함수
 void searchName(Product *s,int count);//제품 명 검색기능 하는 함수
 void searchPrice(Product *s, int count);//가격대 검색하는 함수
