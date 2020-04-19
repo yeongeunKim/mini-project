04deb42250f02db1ecf4e56802d76b0ec3cbdc03
Kim YeongEun <sally4386@naver.com>
2020-04-19 18:30:14 +0900

    add searchStar func in manager.h

--- manager.h
+++ manager.h
@@ -6,4 +6,5 @@ void savdData(Product *s,int count);//파일 저장하는 함수
 int loadData(Product s[]);// 파일 불러오는 함수
 void searchName(Product *s,int count);//제품 명 검색기능 하는 함수
 void searchPrice(Product *s, int count);//가격대 검색하는 함수
+void searchStar(Product *s, int count);//변 점수 검색하는 함수
 
