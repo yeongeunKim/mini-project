2200753fa736d69c2b25a844e2336d33a895bb2a
Kim YeongEun <sally4386@naver.com>
2020-04-19 20:41:04 +0900

    modify product.h

--- product.h
+++ product.h
@@ -13,6 +13,3 @@ int addProduct(Product *s);//상품 추가하는 함수
 void readProduct(Product s);//상품 출력 함수
 int updateProduct(Product *s);// 상품 추가 함수 
 int deleteProduct(Product *s);//상품 삭제 함수
-void saveData(Product *s,int count); //리스트 파일에 저장하는 함수 
-int loadData(Product *s);//파일에 저장된 리스트 불러오는 함수
-int searchProduct(Product *s,int count);//상품 찾는 함수
